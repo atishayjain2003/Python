0706b745c258ca64aacbd2fee8677c7a5c795b32
Atishay Jain <atishayjain2003@gmail.com>
2023-06-01 13:59:04 +0530

    List Swquencing

--- LISTS.py
+++ LISTS.py
@@ -1,18 +1,14 @@
 #Lists are Mutable data types
 #Mutability means values in lists can be changed
 #list sequencing
-
-'''
-
 n=int(input("Enter number of elements in list"))
 L=[]
 print("Enter list elements")
 for i in range(n):
     x=int(input())
     L.append(x)
-
-'''
 #L[start:end:step]
 #the loop runs till end -1
-#L[1::]
+y=L[1::]
+print(y)
 #The above statement means that starting from index 1 all the elements will be displayed
